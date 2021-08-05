a863601fb607cac5482b19ad2d2f0ba2d901302b
abu.bakar <abu.bakar@upgenicsint.work>
2021-08-05 13:24:40 +0500

    Font implemented + clear functionality

--- app/src/main/java/com/app/mamotesttask/MainActivity.kt
+++ app/src/main/java/com/app/mamotesttask/MainActivity.kt
@@ -3,11 +3,18 @@ package com.app.mamotesttask
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tv_totalAmount:AppCompatTextView
+
+    private var total_amount:Long= 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_totalAmount=findViewById(R.id.tv_totalAmount)
     }
     fun onKeypadBtnClick(view: View) {
       /*  val textView: TextView = findViewById(view.id)
@@ -15,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         count++
         setPassCode(inputPassCodeDigit)*/
     }
+
     fun clearPassCode(view: View?) {
 //        count = 0
 //        passCodeFirstDigit.setText("")
@@ -22,5 +30,7 @@ class MainActivity : AppCompatActivity() {
 //        passCodeThirdDigit.setText("")
 //        passCodeFourthDigit.setText("")
 //        passCodeString = ""
+        total_amount=0L
+        tv_totalAmount.text=getString(R.string.aed_0_00)
     }
 }
