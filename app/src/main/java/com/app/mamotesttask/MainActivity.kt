b60ac789f982904668e798e976e93a37b55ab908
abu.bakar <abu.bakar@upgenicsint.work>
2021-08-05 12:07:34 +0500

    KeyPad UI Implemented

--- app/src/main/java/com/app/mamotesttask/MainActivity.kt
+++ app/src/main/java/com/app/mamotesttask/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.app.mamotesttask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onKeypadBtnClick(view: View) {
+      /*  val textView: TextView = findViewById(view.id)
+        val inputPassCodeDigit = textView.text.toString()
+        count++
+        setPassCode(inputPassCodeDigit)*/
+    }
+    fun clearPassCode(view: View?) {
+//        count = 0
+//        passCodeFirstDigit.setText("")
+//        passCodeSecondDigit.setText("")
+//        passCodeThirdDigit.setText("")
+//        passCodeFourthDigit.setText("")
+//        passCodeString = ""
     }
 }
